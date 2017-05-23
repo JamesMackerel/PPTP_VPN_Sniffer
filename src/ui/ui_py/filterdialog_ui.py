ed085ad40d3dae6da7cb1dbf9359f63a6d6ef7cf
JamesMackerel <ghost.mackerel@gmail.com>
2017-05-23 07:05:59 -0700

    make all ui files and *_ui.py files in separate directories.

--- src/ui/ui_py/filterdialog_ui.py
+++ src/ui/ui_py/filterdialog_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file './filterdialog.ui'
+# Form implementation generated from reading ui file './ui_files/filterdialog.ui'
 #
 # Created by: PyQt5 UI code generator 5.5.1
 #
--- src/ui/ui_py/managelogdialog_ui.py
+++ src/ui/ui_py/managelogdialog_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file './managelogdialog.ui'
+# Form implementation generated from reading ui file './ui_files/managelogdialog.ui'
 #
 # Created by: PyQt5 UI code generator 5.5.1
 #
